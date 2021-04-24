1aacdc1b0f28b08cc73ae60132253e6fbf228594
pavinduLakshan <174084F@uom.lk>
2021-04-24 22:21:15 +0530

    centralize form buiding and submission functionality

---
+++ src/utils/index.ts
@@ -0,0 +1,38 @@
+import { RecurringTimeUnit, Week, Month, Year } from "./RecurringTimeUnit"
+import { Customer } from "./Customer"
+import { Payhere } from "../Payhere"
+
+const removeBlankAttributes = (obj: object): object => {
+  return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v != null));
+}
+// +
+const submitPayhereClientRequest = (reqObject: { [x: string]: any; }, requiredParams: any, endpoint: string, onError: (err: string) => void) => {
+  if (!Payhere.merchantId) {
+    onError("Payhere is not initialized")
+    return;
+  }
+  const form = window.document.createElement('form');
+  form.setAttribute('action', Payhere.baseUrl + endpoint);
+  form.style.display = 'none';
+  form.setAttribute('method', 'post');
+  
+  for (const name of Object.keys(reqObject)) {
+    if (reqObject[name]) {
+      const inpt = window.document.createElement('input');
+      inpt.setAttribute('name', name);
+      inpt.setAttribute('type', 'hidden');
+      inpt.setAttribute('value', reqObject[name]);
+      form.appendChild(inpt);
+    } else {
+      if (Object.keys(requiredParams).includes(name)) {
+        onError(`Payhere Error: ${requiredParams[name]} is a required parameter`);
+        return;
+      }
+    }
+  }
+
+  window.document.body.appendChild(form);
+  form.submit();
+}
+
+export { submitPayhereClientRequest, removeBlankAttributes, Week, Month, Year, RecurringTimeUnit, Customer }
