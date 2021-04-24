2d5509cad340b938f2b7d92985e2aa30ba7272ae
pavinduLakshan <174084F@uom.lk>
2021-04-24 22:19:42 +0530

    create separate class to handle customer info

--- src/Payhere.ts
+++ src/Payhere.ts
@@ -1,45 +1,55 @@
+import {Customer} from './utils/Customer'
+
 export enum AccountCategory {
   LIVE,
   SANDBOX,
 }
-export class Payhere {
-  private static merchantId: string;
-  private static baseUrl: string;
-  private static accessCode: string = '';
-  private static authToken: string = '';
 
-  static init(merchantId: string, accountType: AccountCategory, authToken?: string) {
-    this.merchantId = merchantId;
+export class Payhere {
+  private static _merchantId: string;
+  private static _baseUrl: string;
+  private static _accessToken?: string;
+  private static _customerInfo: Customer;
+
+  static init(merchantId: string, accountType: AccountCategory, merchantSecret?: string, customerInfo?: Customer) {
+    this._merchantId = merchantId;
     if (accountType === AccountCategory.LIVE) {
-      this.baseUrl = 'https://www.payhere.lk';
+      this._baseUrl = 'https://www.payhere.lk';
     }
     else if (accountType === AccountCategory.SANDBOX) {
-      this.baseUrl = 'https://sandbox.payhere.lk';
+      this._baseUrl = 'https://sandbox.payhere.lk';
     }
     else throw new Error('Account type is reqquired, but has not been provided. Should be either LIVE or SANDBOX');
-    if (authToken) {
-      Payhere.authToken = authToken;
-      Payhere.accessCode = this.genAccessCode(authToken);
+    if (merchantSecret) {
+      const authCode = this.genAuthCode(merchantSecret);
+      this._accessToken = this.genAccessToken(authCode)
+    }
+    if(customerInfo) {
+      this._customerInfo = customerInfo
     }
   }
 
-  static getMerchantId() {
-    return this.merchantId;
+  static get merchantId(){
+    return Payhere._merchantId;
   }
 
-  static getAccessCode() {
-    return this.accessCode;
+  static get baseUrl(){
+    return Payhere._baseUrl;
   }
 
-  static getAuthToken() {
-    return this.authToken;
+  static get accessToken(){
+    return Payhere._accessToken;
   }
 
-  static getBaseUrl() {
-    return this.baseUrl
+  static get customerInfo(){
+    return Payhere._customerInfo;
   }
 
-  private static genAccessCode(authToken: string): string {
-    return 'access-code';
+  private static genAuthCode(merchantSecret: string): string {
+    return 'auth-code';
+  }
+
+  private static genAccessToken(authorizationCode: string): string {
+    return 'access-token';
   }
 }
